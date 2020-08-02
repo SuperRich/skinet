0c43191f7a92cb49204a8bf82e587a01440626f7
SuperRich <r-hunter@live.co.uk>
2020-08-02 13:36:37 +0100

    Added products page and product details, angular router and lazy loading also added. Nav bar links functioning correctly.

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -3,18 +3,16 @@ import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import {SharedModule} from "../shared/shared.module";
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import {ShopRoutingModule} from "./shop-routing.module";
 
 
-
-// @ts-ignore
 @NgModule({
-  declarations: [ShopComponent, ProductItemComponent],
-  exports: [
-    ShopComponent
-  ],
+  declarations: [ShopComponent, ProductItemComponent, ProductDetailsComponent],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    ShopRoutingModule
   ]
 })
 export class ShopModule { }
