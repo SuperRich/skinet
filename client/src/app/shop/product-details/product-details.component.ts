b5f0b7637d6114f99099fd10166eccaedbd6e434
SuperRich <r-hunter@live.co.uk>
2020-08-05 03:22:19 +0100

    Added loading indicators and applied united bootswatch theme. Also added overlay effect to products.

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -3,6 +3,7 @@ import {IProduct} from "../../shared/models/product";
 import {ShopComponent} from "../shop.component";
 import {ShopService} from "../shop.service";
 import {ActivatedRoute} from "@angular/router";
+import {BreadcrumbService} from "xng-breadcrumb";
 
 @Component({
   selector: 'app-product-details',
@@ -12,7 +13,7 @@ import {ActivatedRoute} from "@angular/router";
 export class ProductDetailsComponent implements OnInit {
   product: IProduct;
 
-  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute) { }
+  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute, private bcService: BreadcrumbService) { }
 
   ngOnInit(): void {
     this.loadProduct();
@@ -21,6 +22,7 @@ export class ProductDetailsComponent implements OnInit {
   loadProduct() {
     this.shopService.getProduct(+this.activatedRoute.snapshot.paramMap.get('id')).subscribe(product => {
       this.product = product;
+      this.bcService.set('@productDetails', product.name)
     }, error => {
       console.log(error);
     })
